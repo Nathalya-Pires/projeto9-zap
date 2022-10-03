69622e9cd5442a3b640ce4f2494c3bf248f8454a
Nathalya-Pires <111201469+Nathalya-Pires@users.noreply.github.com>
2022-10-03 16:12:33 -0400

    Update Flashcards.js

--- src/Flashcards.js
+++ src/Flashcards.js
@@ -1,15 +1,12 @@
 import Questao from "./Questao";
 
-export default function Flashcards({ deckPerguntas }) {
+export default function Flashcards({ deckPerguntas, contador, setContador }) {
     return (
         <>
             {deckPerguntas.map((conj, index) => (
-            <Questao conj={conj} key={index} index={index}/>
+            <Questao conj={conj} key={index} index={index} contador={contador} setContador={setContador}/>
             ))}
 
         </>
     );
 }
-
-
-
