ab0c18684512983c73b40f95b6f142c5737e2ebe
Nathalya-Pires <111201469+Nathalya-Pires@users.noreply.github.com>
2022-10-03 16:11:58 -0400

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,19 +5,7 @@ export default function Footer() {
 
         <Rodape>
 
-            <Botoes>
-                <button>
-                    Não lembrei
-                </button>
-                <button>
-                    Quase não lembrei
-                </button>
-                <button>
-                    Zap!
-                </button>
-            </Botoes>
-
-            0/4 CONCLUÍDOS
+            {contador}/{tamanhoDeck} CONCLUÍDOS
             
         </Rodape>
     )
@@ -38,28 +26,4 @@ const Rodape = styled.div`
   font-size: 18px;
   color: #333333;
   padding: 10px;
-
-  button {
-  width: 90px;
-  font-family: 'Recursive';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  color: #FFFFFF;
-  background: blue;
-  border-radius: 5px;
-  border: 1px solid blue;
-  padding:5px;
-  }
-`
-const Botoes = styled.div`
-  display: flex;
-  width: 80%;
-  justify-content: space-between;
-  margin: 20px;
 `
